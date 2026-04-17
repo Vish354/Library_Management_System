a12f04c7199c545df3d538cc17faf60c6488fc01
Vishal Kumar <vk6375389@gmail.com>
2026-04-17 15:04:00 +0530

    removed mysql and added postgres

--- src/main/java/com/library/management/LibraryManagementSystemApplication.java
+++ src/main/java/com/library/management/LibraryManagementSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EntityScan("com.library.management.entity")
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class LibraryManagementSystemApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(LibraryManagementSystemApplication.class, args);
     }
 }
